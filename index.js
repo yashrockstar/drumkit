a0138921a0fa4974db60d770694e6c286fa9178f
yashrockstar <72990999+yashrockstar@users.noreply.github.com>
2020-10-22 15:50:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 // detecting button press
-
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
 for (var i = 0; i < numberOfDrumButtons; i++)
 {
-
   document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-
      var buttonInnerHTML = this.innerHTML;
-
       makeSound(buttonInnerHTML);
       buttonAnimation(buttonInnerHTML);
     });
-
  }
  // detecting keyboard press
  document.addEventListener("keypress", function (event){
-
-
        makeSound(event.key);
        buttonAnimation(event.key);
-
  });
  //sounds
  function makeSound(key){
@@ -30,80 +21,38 @@ for (var i = 0; i < numberOfDrumButtons; i++)
        var tom1 = new Audio("sounds/tom-1.mp3");
        tom1.play();
        break;
-
      case "a":
        var tom2 = new Audio("sounds/tom-2.mp3");
        tom2.play();
        break;
-
      case "s":
        var tom3 = new Audio('sounds/tom-3.mp3');
        tom3.play();
        break;
-
      case "d":
        var tom4 = new Audio('sounds/tom-4.mp3');
        tom4.play();
        break;
-
      case "j":
        var snare = new Audio('sounds/snare.mp3');
        snare.play();
        break;
-
      case "k":
        var crash = new Audio('sounds/crash.mp3');
        crash.play();
        break;
-
      case "l":
        var kick = new Audio('sounds/kick-bass.mp3');
        kick.play();
        break;
-
-
      default: console.log(key);
-
    }
  }
-
 //Animations
  function buttonAnimation(currentKey){
-
    var activeButton = document.querySelector("." + currentKey);
-
    activeButton.classList.add("pressed");
-
    setTimeout(function() {
      activeButton.classList.remove("pressed");
    }, 100);
  }
-
-
-// function handleclick(){
-//   for(var i=0;i<numberOfDrumButtons;i++)
-//   {
-//       if(document.querySelectorAll(".drum")[i].innerHTML==w)
-//       {
-//            new Audio("sounds/crash.mp3").play();
-//       }
-//    }
-// }
-// function handleclick(){
-//   new Audio("sounds/kick-bass.mp3").play();
-// }
-// function handleclick(){
-//   new Audio("sounds/snare.mp3").play();
-// }
-// function handleclick(){
-//   new Audio("sounds/tom-1.mp3").play();
-// }
-// function handleclick(){
-//   new Audio("sounds/tom-2.mp3").play();
-// }
-// function handleclick(){
-//   new Audio("sounds/tom-3.mp3").play();
-// }
-// function handleclick(){
-//   new Audio("sounds/tom-4.mp3").play();
-// }
